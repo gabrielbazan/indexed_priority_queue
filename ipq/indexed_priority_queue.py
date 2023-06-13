04f6d1749de6b694a1413aba9a380769fced1e11
gabrielbazan <gbazan@outlook.com>
2023-06-13 00:41:47 -0300

    Rename protected methods

--- ipq/indexed_priority_queue.py
+++ ipq/indexed_priority_queue.py
@@ -115,15 +115,15 @@ class IndexedPriorityQueue:
         self._maintain_invariant(index)
 
     def _maintain_invariant(self, index: int) -> None:
-        self._bubble_down(index)
-        self._bubble_up(index)
+        self._move_down(index)
+        self._move_up(index)
 
-    def _bubble_up(self, index) -> None:
+    def _move_up(self, index) -> None:
         parent_index = (index - 1) // 2
 
         if index > 0 and self.queue[parent_index] > self.queue[index]:
             self._swap(index, parent_index)
-            self._bubble_up(parent_index)
+            self._move_up(parent_index)
 
     def _swap(self, index_a: int, index_b: int) -> None:
         key_a = self.index_key[index_a]
@@ -139,7 +139,7 @@ class IndexedPriorityQueue:
         self.key_index[key_a], self.key_index[key_b] = index_b, index_a
         self.index_key[index_a], self.index_key[index_b] = key_b, key_a
 
-    def _bubble_down(self, index: int) -> None:
+    def _move_down(self, index: int) -> None:
         left_child_index = index * 2 + 1
 
         if (
@@ -147,12 +147,13 @@ class IndexedPriorityQueue:
             and self.queue[left_child_index] < self.queue[index]
         ):
             self._swap(index, left_child_index)
-            self._bubble_down(left_child_index)
+            self._move_down(left_child_index)
 
         right_child_index = index * 2 + 2
+
         if (
             right_child_index < len(self.queue)
             and self.queue[right_child_index] < self.queue[index]
         ):
             self._swap(index, right_child_index)
-            self._bubble_down(right_child_index)
+            self._move_down(right_child_index)
